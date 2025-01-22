75660d1a390f961b88a0f3f72fc7036509076ad7
HAJIHYUK <gkwlgurdydydy@naver.com>
2025-01-22 22:29:24 +0900

    깃연동확인커밋

---
+++ QRCodeMaker/src/main/java/com/jh/QrCodeMakerApplication.java
@@ -0,0 +1,13 @@
+package com.jh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeMakerApplication.class, args);
+	}
+
+}
